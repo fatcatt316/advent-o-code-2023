20d6546e87f0338ace2a16b503e271122f4af3a0
Joe Peck <joe@spreedly.com>
2023-12-04 16:12:31 -0500

    Day 3, ay yai yai

--- day03/gondola.rb
+++ day03/gondola.rb
@@ -2,18 +2,16 @@ require 'pry'
 module Gondola
   extend self
 
-  # SYMBOLS = %w(* ? + # $)
-
   def run(filepath = 'test_input.txt')
     schematic = File.readlines(filepath)
-     
+
     part_number_sum = 0
     schematic.each_with_index do |row, row_num|
       current_number = nil
 
-      (0..row.size).each do |col|
+      (0...row.size).each do |col|
 
-        # if current chart is a digit
+        # if current char is a digit
         if row[col] =~ /\d/
           current_number = "#{current_number}#{row[col]}"
 
@@ -38,18 +36,87 @@ module Gondola
     puts part_number_sum
   end
 
+  def gear_ratio(filepath = 'test_input.txt')
+    schematic = File.readlines(filepath)
+
+    gear_ratio_sum = 0
+    schematic.each_with_index do |row, row_num|
+      (0...row.size).each do |col|
+
+        # if current char is a gear
+        if row[col] =~ /\*/
+          adjacent_numbers = find_adjacent_numbers(schematic, row_num, col)
+
+          if adjacent_numbers.size == 2
+            gear_ratio_sum += adjacent_numbers.inject(:*)
+          end
+        end
+      end
+    end
+    puts gear_ratio_sum
+  end
+
+  private def find_adjacent_numbers(schematic, row, col)
+    col_start = [col-1, 0].max
+    min_row = [row-1, 0].max
+
+    adjacent_numbers = []
+
+    (min_row..row+1).each do |row_num|
+      next unless schematic[row_num] && schematic[row_num][col_start..col+1] =~ /\d/
+
+      processed_number = false
+      (col_start..col+1).each do |col|
+        if schematic[row_num][col] =~ /\d/
+          # if haven't already found this number...
+          unless processed_number
+            adjacent_numbers << build_number(schematic[row_num], col)
+            processed_number = true
+          end
+        else
+          processed_number = false
+        end
+      end
+    end
+
+    adjacent_numbers
+  end
+
+  private def build_number(row, index)
+    cur_number = nil
+    this_number = false
+    (0..row.size).each do |col|
+      this_number = true if col == index # we're in the number that we need to return!
+
+      if row[col] =~ /\d/
+        cur_number ||= ''
+        cur_number << row[col]
+      else
+        return cur_number.to_i if this_number
+        cur_number = nil
+      end
+    end
+  end
+
   private def adjacent_to_symbol?(schematic, row, col_start, col_end)
     col_start = [col_start-1, 0].max
     min_row = [row-1, 0].max
 
     (min_row..row+1).each do |row_num|
-      return true if schematic[row_num] && schematic[row_num][col_start..col_end+1] =~ /[^\d\.]/
+      return true if schematic[row_num] && schematic[row_num][col_start..col_end+1].chomp =~ /[^\d\.]/
     end
     false
   end
 end
 
-puts "Should be 4361"
+puts 'Should be 4361'
 Gondola.run
 
+puts 'Should be _'
 Gondola.run('input.txt')
+
+puts 'Should be 467835'
+Gondola.gear_ratio
+
+puts 'Should be _'
+Gondola.gear_ratio('input.txt')
